5871e7bc4487639e82c18e7f6ddf4fa4640949bc
christopher-ridad <157936463+christopher-ridad@users.noreply.github.com>
2025-03-31 23:29:14 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,27 +8,30 @@ public class Main {
                       Blitz Card Game
         ===========================================
         Objective:
-        - Achieve the highest total value of cards of the same suit The highest possible score is 31.
-        - A 10, a face card (Jack, Queen, King), and an Ace of the same suit instantly win!
+        Achieve the highest total value of cards of the same suit. The highest possible score is 31.
     
-       How to Play:
-        1. Turn order is randomized at the start of the game.
-        2. On your turn, choose one of the following:
-        (1) Draw from the deck.
-        (2) Take the top card from the discard pile.
-        (3) Knock
-        3. If you knock, all other players get one more turn.
-        4. The player with the highest suit total at the end of the game wins!
+        How to Play:
+        Each player is dealt 3 cards in their hand. At the start of the game, turn order is automatically randomized.
+        On your turn, you can do one of the following:
+        - Draw from the deck
+        - Take the top card from the discard pile
+        - Knock        
 
-         Card Values:
-        - Number cards: Face value (e.g., 2 of Hearts = 2 points).
-        - Face cards (Jack, Queen, King): 10 points each.
-        - Aces: 11 points.
+        If a player knocks, all other players get one more turn, and then the game ends. 
+        The game also automatically ends if there are no more cards to draw from in the deck. 
+        The player with the highest suit total at the end of the game wins!
+
+        Card Values:
+        - Number cards: Face value (e.g., 2 of Hearts = 2 points)
+        - Face cards (Jack, Queen, King): 10 points each
+        - Aces: 11 points
 
         Scoring:
         - Your score is determined by the highest total value of cards from a single suit.
         - All cards of the same suit in your hand are added together.
-        - The game will immediately end when a player has a 10, a face card, and an Ace of the same suit in their hand. It's an instant win!
+        
+        Automatic Win Condition:
+        The game will immediately end when a player has a 10, a face card, and an Ace of the same suit in their hand. It's an instant win!
 
         Get ready to play!    
         ===========================================
