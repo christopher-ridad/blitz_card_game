4024efdffe4eddb9bca30e273e4ec922c07d85dc
nani-pardi <161642342+nani-pardi@users.noreply.github.com>
2025-06-08 21:01:04 -0500

    refactoring

--- src/presentation/GameView.java
+++ src/presentation/GameView.java
@@ -1,11 +1,5 @@
 package presentation;
 
 public class GameView {
-    public void displayGameSetup() {
-        System.out.println("Setting up the game...");
-    }
 
-    public void displayEndScreen() {
-        System.out.println("Game over! Thanks for playing.");
-    }
 }
