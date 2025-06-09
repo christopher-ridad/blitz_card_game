95ec365719ade4f2b7da743b1e97310b4c235576
nani-pardi <161642342+nani-pardi@users.noreply.github.com>
2025-06-08 19:19:20 -0500

    Implemented SimpleAiStrategy.java

--- src/domain/player/SimpleAIStrategy.java
+++ src/domain/player/SimpleAIStrategy.java
@@ -3,37 +3,50 @@ package src.domain.player;
 import src.domain.blitzengine.Blitz;
 import src.domain.blitzengine.Move;
 import src.domain.cards.Card;
+import src.domain.blitzengine.PlayerTurn;
+import src.domain.blitzengine.BlitzScoringStrategy;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class SimpleAIStrategy implements AIStrategy {
     private Blitz blitz;
 
-    public Move makeMoveDecision(Hand hand) throws NoMoveDecisionException {
-        Card topCard = blitz.getTopOfDiscardPile();
+    public SimpleAIStrategy(Blitz blitz){
+        this.blitz = blitz;
+    }
 
-        List<Card> currentHand = hand.getCards(); // You’ll need this method in Hand
-        Card discardCard = topCard;
-        int bestScore = hand.calculateScore();
+    @Override
+    public Move makeMoveDecision(Hand hand) throws NoMoveDecisionException {
+        Card topCard = blitz.getLastMoveMade().getCardDiscarded();
+
+        List<Card> currentHand = new ArrayList<>(hand.getCards()); // You’ll need this method in Hand
+        Card discardCandidate = topCard;
+        int bestScore = hand.getScore();
 
         for (Card card : currentHand) {
             List<Card> simulated = new ArrayList<>(currentHand);
             simulated.remove(card);
             simulated.add(topCard);
-            int simulatedScore = hand.calculateScore(simulated);
+            int simulatedScore =  new BlitzScoringStrategy().calculateScore(simulated);
 
             if (simulatedScore > bestScore) {
                 bestScore = simulatedScore;
-                discardCard = card;
+                discardCandidate = card;
             }
         }
 
         if (bestScore >= 27) {
-            return new Move("Knock");
+            blitz.knock();
+            return new Move(PlayerTurn.KNOCK, null, null, null, new Date());
         }
 
-        hand.remove(discardCard);
-        hand.add(discardPile.drawTopCard());
-        return new Move("Discard", discardCard);
+        blitz.drawCardFromDiscardPile();
+        Card drawnCard = topCard;
+        hand.removeCard(discardCandidate);
+        hand.addCard(drawnCard);
+        return new Move(PlayerTurn.DRAW_CARD_FROM_DISCARD_PILE, null, drawnCard, discardCandidate, new Date());
     }
 
     @Override
@@ -41,31 +54,5 @@ public class SimpleAIStrategy implements AIStrategy {
 
     }
 
-//    public Card improveHand(Card newCard) {
-//        Card discardCard = newCard;
-//        int simulateScore = getScore();
-//        for(Card card : this.hand){
-//            int newScore = simulateHand(newCard, card); // Simualate the hand with every combination of cards of the hand and the newCard
-//            if(newScore > simulateScore){ // Determines which card to discard to maximize the hand's score
-//                discardCard = card;
-//                simulateScore = newScore;
-//            }
-//        }
-//        return discardCard; // Return the card to be discarded
-//    }
-//
-//    // simulateHand(src.domain.cards.Card newCard, src.domain.cards.Card discardCard): Simulates the hand by adding a new card and removing a current card in the hand
-//    private int simulateHand(Card newCard, Card discardCard){
-//        ArrayList<Card> tempHand = new ArrayList<>();
-//        tempHand.addAll(this.hand); // Copy the current hand
-//        tempHand.remove(discardCard); // Remove the card to be discarded
-//        tempHand.add(newCard); // Add the new card
-//        return calculateScore(tempHand); // Calculate the score of the simulated hand
-//    }
-//
-//    // shouldKnock(): Determines if the AI player should knock based on the strength of their hand
-//    public boolean shouldKnock() {
-//        // The AI player will knock if their score is 27 or higher
-//        return getScore() >= 27;
-//    }
 }
+
